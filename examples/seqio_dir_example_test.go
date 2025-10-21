df6c9bbbc94ef427d07f77979fef43efa8ecc01f
Gord Allott <gord@axiom.co>
2025-10-21 14:37:40 +0000

    add dir functions

---
+++ examples/seqio_dir_example_test.go
@@ -0,0 +1,10 @@
+package examples
+
+import (
+	"testing"
+)
+
+func TestExampleSeqioDirOperations(t *testing.T) {
+	// TODO: Create a sample directory structure for demonstration
+	// TODO: add an example
+}
